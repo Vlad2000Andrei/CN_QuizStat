719237f88f323bee36ae083b195b7d32330a2b4f
Vlad <cursaru.vlad@gmail.com>
2024-05-06 11:06:31 +0200

    Removed legacy code

--- setup.ps1
+++
@@ -1,4 +0,0 @@
-mkdir venv
-python3 -m venv .\venv
-.\venv\Scripts\Activate.ps1
-pip install -r .\requirements.txt
--- setup.sh
+++
@@ -1,4 +0,0 @@
-mkdir venv
-python3 -m venv ./venv
-bash ./venv/Scripts/activate
-pip install -r .\requirements.txt
